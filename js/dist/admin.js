8aee1ea472943ee9b2acba5743d6f51242346b0b
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-06-12 15:40:35 +0200

    Update admin.js

--- js/dist/admin.js
+++ js/dist/admin.js
@@ -1,16 +1,20 @@
-flarum.extensions['peopleinside-first-post-approval'] = function() {
-  const app = flarum.core.app;
-  app.initializers.add('peopleinside-first-post-approval', function() {
-    app.extensionData
-      .for('peopleinside-first-post-approval')
-      .registerSetting({
-        setting: 'peopleinside-first-post-approval.approval_type',
-        label: app.translator.trans('peopleinside-first-post-approval.admin.settings.approval_type_label'),
-        type: 'select',
-        options: {
-          'all': app.translator.trans('peopleinside-first-post-approval.admin.settings.all_posts'),
-          'first': app.translator.trans('peopleinside-first-post-approval.admin.settings.first_post_only'),
-        }
-      });
-  });
+flarum.extensions['peopleinside-flarum-ext-first-post-approval'] = {
+  extend: [
+    {
+      extend: function(app) {
+        app.extensionData
+          .for('peopleinside-flarum-ext-first-post-approval')
+          .registerSetting({
+            setting: 'peopleinside-first-post-approval.approval_type',
+            label: app.translator.trans('peopleinside-first-post-approval.admin.settings.approval_type_label'),
+            type: 'select',
+            options: {
+              'all': app.translator.trans('peopleinside-first-post-approval.admin.settings.all_posts'),
+              'first': app.translator.trans('peopleinside-first-post-approval.admin.settings.first_post_only'),
+            },
+            help: app.translator.trans('peopleinside-first-post-approval.admin.settings.approval_type_help'),
+          });
+      }
+    }
+  ]
 };
