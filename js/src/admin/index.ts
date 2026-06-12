e8c6d92bfc6cace10176d3445663d531d4185b9f
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-06-12 15:37:59 +0200

    Update index.ts

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,23 +1,15 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('peopleinside-first-post-approval', () => {
-    app.extensionData
-        .for('peopleinside-first-post-approval')
-        .registerSetting({
-            setting: 'clarkwinkelmann-first-post-approval.postCount',
-            type: 'number',
-            label: app.translator.trans('peopleinside-first-post-approval.admin.settings.postCount'),
-            min: 0,
-        })
-        .registerSetting({
-            setting: 'clarkwinkelmann-first-post-approval.discussionCount',
-            type: 'number',
-            label: app.translator.trans('peopleinside-first-post-approval.admin.settings.discussionCount'),
-            min: 0,
-        })
-        .registerPermission({
-            icon: 'fas fa-check',
-            label: app.translator.trans('peopleinside-first-post-approval.admin.permissions.bypass'),
-            permission: 'discussion.firstPostWithoutApproval',
-        }, 'start');
+  app.extensionData
+    .for('peopleinside-first-post-approval')
+    .registerSetting({
+      setting: 'peopleinside-first-post-approval.approval_type',
+      label: app.translator.trans('peopleinside-first-post-approval.admin.settings.approval_type_label'),
+      type: 'select',
+      options: {
+        'all': app.translator.trans('peopleinside-first-post-approval.admin.settings.all_posts'),
+        'first': app.translator.trans('peopleinside-first-post-approval.admin.settings.first_post_only'),
+      },
+    });
 });
