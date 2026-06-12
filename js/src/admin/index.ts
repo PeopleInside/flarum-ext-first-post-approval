5fb137a4469cc9f4cd2d241d756181400b31e250
peopleinside-it <285456092+peopleinside-it@users.noreply.github.com>
2026-06-12 15:52:12 +0200

    fix 2.0.2

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,23 +1,23 @@
 import app from 'flarum/admin/app';
 
-app.initializers.add('peopleinside-first-post-approval', () => {
+app.initializers.add('peopleinside-flarum-ext-first-post-approval', () => {
     app.extensionData
-        .for('peopleinside-first-post-approval')
+        .for('peopleinside-flarum-ext-first-post-approval')
         .registerSetting({
             setting: 'clarkwinkelmann-first-post-approval.postCount',
             type: 'number',
-            label: app.translator.trans('peopleinside-first-post-approval.admin.settings.postCount'),
+            label: app.translator.trans('peopleinside-flarum-ext-first-post-approval.admin.settings.postCount'),
             min: 0,
         })
         .registerSetting({
             setting: 'clarkwinkelmann-first-post-approval.discussionCount',
             type: 'number',
-            label: app.translator.trans('peopleinside-first-post-approval.admin.settings.discussionCount'),
+            label: app.translator.trans('peopleinside-flarum-ext-first-post-approval.admin.settings.discussionCount'),
             min: 0,
         })
         .registerPermission({
             icon: 'fas fa-check',
-            label: app.translator.trans('peopleinside-first-post-approval.admin.permissions.bypass'),
+            label: app.translator.trans('peopleinside-flarum-ext-first-post-approval.admin.permissions.bypass'),
             permission: 'discussion.firstPostWithoutApproval',
         }, 'start');
 });
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,4 +1,1 @@
-const path = require('path');
-process.chdir(__dirname);
 module.exports = require('flarum-webpack-config')();
-
