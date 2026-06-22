62879854a5ad44ee3a0e0c3009e8c99558102aed
Marco Borla <5006150+PeopleInside@users.noreply.github.com>
2026-06-22 13:29:49 +0200

    Update SyncDiscussionHidden.php

--- src/Listeners/SyncDiscussionHidden.php
+++ src/Listeners/SyncDiscussionHidden.php
@@ -3,18 +3,10 @@
 namespace PeopleInside\FirstPostApproval\Listeners;
 
 use Flarum\Discussion\Event\Hidden;
-use Illuminate\Database\ConnectionInterface;
 
 class SyncDiscussionHidden
 {
     use SyncsUserCounts;
-
-    protected $db;
-
-    public function __construct(ConnectionInterface $db)
-    {
-        $this->db = $db;
-    }
 
     public function handle(Hidden $event)
     {
